10364e1b65f0d1f73f93946c5063fdaaae414a8a
Shivank12batra <Shivank56batra@gmail.com>
2023-01-31 23:21:23 +0530

    tailwind css reconfigured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+function App() { 
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="text-4xl font-bold">Hello</h1>
     </div>
   )
-}
+} 
 
 export default App;
