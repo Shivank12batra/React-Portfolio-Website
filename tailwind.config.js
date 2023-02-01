2dd0a7b9358c22f1ab5d242e52af617363ae5354
Shivank12batra <Shivank56batra@gmail.com>
2023-02-02 02:55:41 +0530

    content configuration done inside tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html, js, jsx}", 
+    "./src/**/*"
+  ],
   theme: {
     extend: {
       fontFamily: {
