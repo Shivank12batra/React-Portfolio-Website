6efa423a80ab1a1e4af73e8abbb35e149cd420cc
Shivank12batra <Shivank56batra@gmail.com>
2023-02-01 12:47:38 +0530

    google custom fonts included in tailwind

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 function App() { 
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello</h1>
+      <h1 className="text-4xl font-bold font-signature">Hello</h1> 
     </div>
   )
 } 
 
-export default App;
+export default App; 
