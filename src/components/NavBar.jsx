c873632783477466449b41dd8384c90cc8e1da47
Shivank12batra <Shivank56batra@gmail.com>
2023-02-02 16:08:55 +0530

    screens configuration added to tailwind.config + basic navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,40 @@ import React from 'react'
 // import {FaBars, FaTimes} from "react-icons/fa"
 
 const NavBar = () => {
+    const links = [
+        {
+            id: 1,
+            link: 'home'
+        },
+        {
+            id: 2,
+            link: 'about'
+        },
+        {
+            id: 3,
+            link: 'projects'
+        },
+        {
+            id: 4,
+            link: 'blogs'
+        },
+        {
+            id: 5,
+            link: 'contact'
+        },
+    ]
   return (
     <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed">
         <div>
-            <h1 className='font-bold'>Shivank</h1>
+            <h1 className='text-4xl ml-2 font-signature'>Shivank</h1>
         </div>
+        <ul className='hidden md:flex'>
+            {links.map(({id, link}) => {
+                return <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+                    {link}
+                </li>
+            })}
+        </ul>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*"
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
+      animations: {
+        'scale-105': {
+          transform: 'scale(1.05)'
+        },
+      },
+      transitionDuration: {
+        '200': '200ms',
+      },
       fontFamily: {
         signature: "'Great Vibes', cursive"
       }
