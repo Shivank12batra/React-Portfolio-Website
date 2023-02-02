b884df7611bb2d67d283301843f7d8b3bf9fbf21
Shivank12batra <Shivank56batra@gmail.com>
2023-02-02 21:53:00 +0530

    navbar done for both mobile and desktop view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-// import {FaBars, FaTimes} from "react-icons/fa"
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from "react-icons/fa"
 
 const NavBar = () => {
+
+    const [nav, setNav] = useState(false)
+
     const links = [
         {
             id: 1,
@@ -36,6 +39,18 @@ const NavBar = () => {
                 </li>
             })}
         </ul>
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-grey-500 md:hidden'>
+            {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
+        </div>
+        {nav && (
+            <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+                {links.map(({id, link}) => {
+                    return <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4x">
+                        {link}
+                    </li>
+                })}
+            </ul>
+        )}
     </div>
   )
 }
