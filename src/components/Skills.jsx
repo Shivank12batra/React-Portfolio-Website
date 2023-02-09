1e03c9e094e6322c9ca0af9814dba057c8a1337e
Shivank12batra <Shivank56batra@gmail.com>
2023-02-09 23:36:15 +0530

    overlapping skills section issue fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -56,11 +56,11 @@ const Skills = () => {
             id: 8,
             src: git,
             name: 'Git/Version Control',
-            style: 'shadow-grey-500'
+            style: 'shadow-orange-500'
         },
     ]
   return (
-    <div className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div className='bg-gradient-to-b from-gray-800 to-black w-full'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Skills</p>
