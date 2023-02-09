b213f519bead51a8f796180c1c338f824152b190
Shivank12batra <Shivank56batra@gmail.com>
2023-02-10 02:32:48 +0530

    overlapping divs issues fixed for mobile screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,11 @@ const About = () => {
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-        <p className='text-xl mt-20'>
+        <p className='sm:text-xl mt-5'>
         Hello and welcome to my world! I am Shivank Batra, a curious soul with a fascination for uncovering abstractions. I am currently on a journey to master the frontend ecosystem, specifically with React JS, and I'm continuously honing my skills by tackling algorithm problems on LeetCode.
         </p>
         <br/>
-        <p className='text-xl'>
+        <p className='sm:text-xl'>
         When I'm not coding, I love to indulge in some sports and games like football and chess. And when I need to energize my mind and soul, I listen to some heavy metal songs, which are a blast! So, if you're looking for someone who's passionate, driven, and always eager to learn, I'm your guy. Let's create some magic together! 
         </p>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
         },
     ]
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-10 md:h-20 text-white bg-black fixed">
         <div>
             <h1 className='text-4xl ml-2 font-signature'>Shivank</h1>
         </div>
