e1ce170a35b8900c1216daa803a1a93d955cbb71
Shivank12batra <Shivank56batra@gmail.com>
2023-02-14 13:11:39 +0530

    text white on hover added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
         </div>
         <ul className='hidden md:flex'>
             {links.map(({id, link}) => {
-                return <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+                return <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200">
                     <Link to={link} smooth duration={500}>{link}</Link>
                 </li>
             })}
@@ -46,7 +46,7 @@ const NavBar = () => {
         {nav && (
             <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
                 {links.map(({id, link}) => {
-                    return <li key={id} className="px-4 cursor-pointer capitalize py-6 text-3xl">
+                    return <li key={id} className="px-4 cursor-pointer capitalize py-6 text-3xl hover:text-white">
                         <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
                     </li>
                 })}
