40570e71d1422e19a3e14f7ed3f988eb835c6162
Shivank12batra <Shivank56batra@gmail.com>
2023-02-14 13:08:22 +0530

    linktree link added in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,8 @@ const About = () => {
         <p className='sm:text-xl'>
         When I'm not coding, I love to indulge in some sports and games like football and chess. And when I need to energize my mind and soul, I listen to some heavy metal songs, which are a blast! So, if you're looking for someone who's passionate, driven, and always eager to learn, I'm your guy. Let's create some magic together! 
         </p>
+        <br/>
+        <p className='sm:text-xl'>To know more about me: <a className='font-bold underline' target='_blank' rel='noreferrer' href='https://linktr.ee/shivank12batra'>Click here!</a></p>
     </div>
   </div>)
 }
